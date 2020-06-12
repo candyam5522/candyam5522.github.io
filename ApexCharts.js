8faaee2bd37ba58054814f6d242cfc0e44c3a50c
Miao Ai <miaoai2010@gmail.com>
2020-06-12 16:23:00 -0400

    revert change

--- ApexCharts.js
+++ ApexCharts.js
@@ -6178,13 +6178,7 @@
             }, {
                 key: "exportToSVG",
                 value: function() {
-                    //this.triggerDownload(this.svgUrl(), ".svg")
-                    var t = this;
-                    this.dataURI().then((function(e) {
-                        var i = e.imgURI,
-                            a = e.blob;
-                        a ? navigator.msSaveOrOpenBlob(a, t.w.globals.chartID + ".svg") : t.triggerDownload(i, ".svg")
-                    }))
+                    this.triggerDownload(this.svgUrl(), ".svg")
                 }
             }, {
                 key: "exportToPng",
